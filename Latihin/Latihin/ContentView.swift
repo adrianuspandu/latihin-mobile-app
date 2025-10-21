3958d60bd08aefcb10c9dfa871cf241e87f7129d
Adrianus Pandu Wicaksono <pwicaks@gmail.com>
2025-10-21 15:24:52 +0200

    first commit

---
+++ Latihin/Latihin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Latihin
+//
+//  Created by Adrianus Pandu Wicaksono on 21.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Latihin/Latihin/LatihinApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatihinApp.swift
+//  Latihin
+//
+//  Created by Adrianus Pandu Wicaksono on 21.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct LatihinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
