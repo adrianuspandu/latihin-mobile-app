e0c3d110624018543a2522415d310720569ddd44
Adrianus Pandu Wicaksono <pwicaks@gmail.com>
2025-11-08 09:51:10 +0100

    feat: Created SignInView

--- Latihin/Latihin/Views/SignInView.swift
+++ Latihin/Latihin/Views/SignInView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct SignInView: View {
+    @State var email = ""
+    @State var password = ""
+    @State var isValid = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            Spacer()
+            
+            VStack(spacing: 20) {
+                
+                LTHTextField(text: $email, label: "Email address", type: .email, placeholder: "johnappleseed@example.com", errorMessage: "Please enter a valid email address.")
+                
+                LTHTextField(text: $password, label: "Password", type: .password, errorMessage: "Incorrect Password")
+            }
+            
+            Spacer()
+            
+            LTHButton(title: "Sign in", variant: isValid ? .primary : .disabled) {
+                // Action here
+            }
+            
+            Spacer().frame(height: 40)
+            
+            HStack {
+                Text("Don't have an account?")
+                NavigationLink(destination: CreateAccountView()) {
+                    Text("Create account")
+                        .foregroundStyle(Color(.systemBlue))
+                }
+            }
+            
+        }
+        .navigationTitle("Sign In")
+        .navigationBarTitleDisplayMode(.inline)
+        .padding(20)
+        
     }
 }
 
