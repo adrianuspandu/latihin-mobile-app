f9aa3e8c4b34752af4322786ded76a5195bf069b
Adrianus Pandu Wicaksono <pwicaks@gmail.com>
2025-11-08 13:29:04 +0100

    Revert "fix: NavigationLink in WelcomeView"
    This reverts commit c326f922496cb7b46ba6983d44e0979ef4bfc3b6.

--- Latihin/Latihin/Views/WelcomeView.swift
+++ Latihin/Latihin/Views/WelcomeView.swift
@@ -34,12 +34,11 @@ struct WelcomeView: View {
                 
                 // MARK: Buttons
                 VStack(alignment: .center, spacing: 20) {
-                    NavigationLink(destination: CreateAccountView()) {
-                        LTHButtonNavLink(title: "Create account", variant: .primary)
+                    LTHButton(title: "Create account", variant: .primary) {
+                        // Create account action here
                     }
-                    
-                    NavigationLink(destination: SignInView()) {
-                        LTHButtonNavLink(title: "Sign in", variant: .secondary)
+                    LTHButton(title: "Sign in", variant: .secondary) {
+                        // Sign in action here
                     }
                 }
             }
