b0b140c1002d9971c6994c410815f195fd998e5a
Adrianus Pandu Wicaksono <pwicaks@gmail.com>
2025-11-08 09:44:51 +0100

    Revert "feat: Created SignInView"
    This reverts commit e1f00bfb51684d0966f3eb4c1e7bc8acd7507eff.

--- Latihin/Latihin/Views/SignInView.swift
+++ Latihin/Latihin/Views/SignInView.swift
@@ -8,40 +8,8 @@
 import SwiftUI
 
 struct SignInView: View {
-    @State var email = ""
-    @State var password = ""
-    @State var isValid = false
-    
     var body: some View {
-        VStack {
-            Spacer()
-            
-            VStack(spacing: 20) {
-                LTHTextField(text: $email, label: "Email address", type: .email, placeholder: "johnappleseed@example.com", errorMessage: "Please enter a valid email address.")
-                
-                LTHTextField(text: $password, label: "Password", type: .password, errorMessage: "Password must be at least 8 characters long, include at least one uppercase letter, one lowercase letter, one digit, and one special character.")
-            }
-            
-            Spacer()
-            
-            LTHButton(title: "Sign in", variant: isValid ? .primary : .disabled) {
-                // Action here
-            }
-            
-            Spacer().frame(height: 40)
-            
-            HStack {
-                Text("Don't have an account?")
-                NavigationLink(destination: CreateAccountView()) {
-                    Text("Create account")
-                        .foregroundStyle(Color(.systemBlue))
-                }
-            }
-            
-        }
-        .navigationTitle("Sign In")
-        .navigationBarTitleDisplayMode(.inline)
-        .padding(20)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
